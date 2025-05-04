f9b9f31bb13bdc4023fbc9dd31c40a16d94b23f8
AlphaIsYour <alphrenoorz@gmail.com>
2025-05-04 17:30:28 +0700

    fixing editjenjang.php

--- admin/editjenjang.php
+++ admin/editjenjang.php
@@ -1,3 +1,29 @@
+<?php
+session_start();
+include('../koneksi/koneksi.php');
+if(isset($_GET['data'])){
+    $id_master_jenjang = mysqli_real_escape_string($koneksi, $_GET['data']);
+    $_SESSION['id_master_jenjang'] = $id_master_jenjang;
+    
+    // Get university data using prepared statement
+    $sql_d = "SELECT `jenjang` FROM `master_jenjang` WHERE `id_master_jenjang` = ?";
+    $stmt = mysqli_prepare($koneksi, $sql_d);
+    mysqli_stmt_bind_param($stmt, 's', $id_master_jenjang);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    
+    if(mysqli_num_rows($result) > 0) {
+        $data_d = mysqli_fetch_assoc($result);
+        $jenjang = $data_d['jenjang'];
+    } else {
+        // Redirect if university doesn't exist
+        header("Location: jenjang.php");
+        exit;
+    }
+    mysqli_stmt_close($stmt);
+}
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -16,12 +42,12 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h3><i class="fas fa-edit"></i> Edit Tag</h3>
+            <h3><i class="fas fa-edit"></i> Edit Jenjang</h3>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="katgoribuku.php">Tag</a></li>
-              <li class="breadcrumb-item active">Edit Tag</li>
+              <li class="breadcrumb-item"><a href="jenjang.php">jenjang</a></li>
+              <li class="breadcrumb-item active">Edit Jenjang</li>
             </ol>
           </div>
         </div>
@@ -33,23 +59,31 @@
 
     <div class="card card-info">
       <div class="card-header">
-        <h3 class="card-title"style="margin-top:5px;"><i class="far fa-list-alt"></i> Form Edit Tag</h3>
+        <h3 class="card-title"style="margin-top:5px;"><i class="far fa-list-alt"></i> Form Edit Jenjang</h3>
         <div class="card-tools">
-          <a href="tag.php" class="btn btn-sm btn-warning float-right"><i class="fas fa-arrow-alt-circle-left"></i> Kembali</a>
+          <a href="jenjang.php" class="btn btn-sm btn-warning float-right"><i class="fas fa-arrow-alt-circle-left"></i> Kembali</a>
         </div>
       </div>
       <!-- /.card-header -->
       <!-- form start -->
       </br>
-      <div class="col-sm-10">
-          <div class="alert alert-danger" role="alert">Maaf data Tag wajib di isi</div>
-      </div>
-      <form class="form-horizontal">
+      <?php if(!empty($_GET['notif'])){?>
+        <?php if($_GET['notif']=="editkosong"){?>
+          <div class="alert alert-danger" role="alert">
+            Maaf data jenjang wajib di isi
+          </div>
+        <?php } else if($_GET['notif']=="editgagal"){?>
+          <div class="alert alert-danger" role="alert">
+            Maaf nama jenjang sudah ada
+          </div>
+        <?php }?>
+      <?php }?>
+      <form class="form-horizontal" method="post" action="konfirmasiedituniversitas.php">
         <div class="card-body">
           <div class="form-group row">
-            <label for="Tag" class="col-sm-3 col-form-label">Tag</label>
+            <label for="jenjang" class="col-sm-3 col-form-label">Jenjang</label>
             <div class="col-sm-7">
-              <input type="text" class="form-control" id="Tag" name="tag" value="PHP">
+              <input type="text" class="form-control" id="jenjang" name="jenjang" value="<?php echo htmlspecialchars($jenjang);?>">
             </div>
           </div>
         </div>
