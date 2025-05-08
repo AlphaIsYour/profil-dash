f6581b8268aeff494f843f317605f0074078636c
AlphaIsYour <alphrenoorz@gmail.com>
2025-05-08 09:25:11 +0700

    double check

--- admin/edituniversitas.php
+++ admin/edituniversitas.php
@@ -1,9 +1,8 @@
 <?php
-// Tidak perlu session_start()
-include('../koneksi/koneksi.php'); // Sesuaikan path
+include('../koneksi/koneksi.php');
 
 $id_master_universitas = null;
-$nama_universitas_lama = ''; // Default
+$nama_universitas_lama = '';
 
 // Validasi dan ambil ID dari URL
 if (isset($_GET['data']) && filter_var($_GET['data'], FILTER_VALIDATE_INT)) {
